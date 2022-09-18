a1e0d981bccc02e25ad675d6c20c65fc4e48d8bb
David-OConnor <david.alan.oconnor@gmail.com>
2022-09-18 04:38:38 +0000

    New bond angle system now working.

--- src/bond_vecs.rs
+++ src/bond_vecs.rs
@@ -14,17 +14,22 @@ pub const LEN_CALPHA_CP: f64 = 1.53; // angstrom
 pub const LEN_CP_O: f64 = 1.2; // angstrom // todo placeholder!
 pub const LEN_N_H: f64 = 1.0; // angstrom // todo placeholder!
 
+
 // Ideal bond angles. There are an approximation; from averages. Consider replacing with something
 // more robust later. All angles are in radians. We use degrees with math to match common sources.
 // R indicates the side chain.
-pub const BOND_ANGLE_N_CP_CALPHA: f64 = 121.7 * TAU / 360.; // This is to Calpha and C'.
-pub const BOND_ANGLE_N_CP_H: f64 = 120. * TAU / 360.; // todo: Placeholder; not real data
+// Our convention is to base each angle off the anchor bond.
+
+// todo: N is listed as 121.7 or 122.7 depending on which side??
+pub const BOND_ANGLE_N_CALPHA_CP: f64 = 122.7 * TAU / 360.; // This is to Calpha and C'.
+pub const BOND_ANGLE_N_CALPHA_H: f64 = 120. * TAU / 360.; // todo: Placeholder; not real data
 
 // Bond from the Calpha atom
-pub const BOND_ANGLE_CALPHA_N_R: f64 = 110.6 * TAU / 360.;
-// pub const BOND_ANGLE_CALPHA_R_CP: f64 = 110.6 * TAU / 360.;
-pub const BOND_ANGLE_CALPHA_N_CP: f64 = 111.0 * TAU / 360.;
-pub const BOND_ANGLE_CALPHA_N_H: f64 = 109.5 * TAU / 360.; // todo: Placeholder; Find this.
+pub const BOND_ANGLE_CALPHA_CP_R: f64 = 110.6 * TAU / 360.;
+pub const BOND_ANGLE_CALPHA_CP_N: f64 = 111.0 * TAU / 360.;
+pub const BOND_ANGLE_CALPHA_CP_H: f64 = 109.5 * TAU / 360.; // todo: Placeholder; Find this.
+pub const BOND_ANGLE_CALPHA_N_R: f64 = 110.6 * TAU / 360.; // todo: Placeholder; Find this.
+
 
 // todo: It would be more consistent to base all bonds off one (eg N), but the value we
 // todo found for the sidechain is from CP. Ideally, get CALPHA_N_R.
@@ -32,10 +37,9 @@ pub const BOND_ANGLE_CALPHA_N_H: f64 = 109.5 * TAU / 360.; // todo: Placeholder;
 // Bonds from the C' atom
 // Note that these bonds add up to exactly 360, so these must be along
 // a circle (in the same plane).
+pub const BOND_ANGLE_CP_N_CALPHA: f64 = 117.2 * TAU / 360.;
+pub const BOND_ANGLE_CP_N_O: f64 = 122.7 * TAU / 360.;
 pub const BOND_ANGLE_CP_CALPHA_O: f64 = 120.1 * TAU / 360.;
-pub const BOND_ANGLE_CP_CALPHA_N: f64 = 117.2 * TAU / 360.;
-pub const BOND_ANGLE_CP_O_N: f64 = 122.7 * TAU / 360.;
-// todo: c' hydrogen
 
 // An arbitrary vector that anchors the others
 pub const ANCHOR_BOND_VEC: Vec3 = Vec3 {
@@ -112,10 +116,55 @@ pub static mut SIDECHAIN_BOND_OUT2: Vec3 = Vec3 {
     z: 0.,
 };
 
-/// Find the next vector in a plane, given a starting vector, and rotation angle.
-fn find_vec_in_plane(start: Vec3, angle: f64, plane_norm: Vec3) -> Vec3 {
-    let rotation = Quaternion::from_axis_angle(plane_norm, angle);
+/// Rotate a vector in 3d a given angle, given a rotation plane.
+fn rotate_vec(start: Vec3, angle: f64, rot_plane_norm: Vec3) -> Vec3 {
+    let rotation = Quaternion::from_axis_angle(rot_plane_norm, angle);
     rotation.rotate_vec(start)
+}
+
+/// Find the third bond by iterating over rotation axes, and using the one that provides
+/// the closest match. For each rotation axis, we apply one bond angle as a constraint, and
+/// attempt to minimize the second one.
+/// The initial rotation plane norm is the plane used to find bond2 by rotating bond1 around it.
+fn find_third_bond_vec(
+    bond1: Vec3,
+    bond2: Vec3,
+    angle_1_3: f64,
+    angle_2_3: f64,
+    bond2_rot_plane_norm: Vec3,
+) -> Vec3 {
+    // The angle between bond 2 and 3 must be within this (radians) to find our result.
+    const EPS: f64 = 0.05;
+    // The number of rotation plane to try.
+    const N_PLANES: usize = 60;
+
+    let rot_plane_incr = TAU / N_PLANES as f64;
+
+    let mut result = Vec3::new_zero();
+
+    // Rotate vec1 along different rotation planes, starting with the one we used to find bond2.
+    // This approximates the set of vectors orthonormal to the anchor vec (bond1)
+    for i in 0..N_PLANES {
+        let iterated_rot_plane_norm =
+            rotate_vec(bond2_rot_plane_norm, rot_plane_incr * i as f64, bond1);
+
+        // Rotate bond 1 around the angle between bond 1 and 3, along our iterated rotation angle.
+        result = rotate_vec(bond1, angle_1_3, iterated_rot_plane_norm);
+        println!("RES: {:?}", result);
+
+        // Measure the other constraint; the angle between bond 1 and our bond3 candidate.
+        let angle_2_3_meas = (bond2.dot(result)).acos();
+
+        println!("ANGLE MEAS: {:?}", angle_2_3_meas);
+
+        // Compare the other constraint against the target; the bond 1-3 angle.
+        if (angle_2_3_meas - angle_2_3).abs() < EPS {
+            println!("FOUND IT");
+            break;
+        }
+    }
+
+    result
 }
 
 /// Calculate local bond vectors based on relative angles, and arbitrary constants.
@@ -135,72 +184,58 @@ pub fn init_local_bond_vecs() {
 
     // We use this normal plane for all rotations if the bond are in plane. We use it for
     // the first rotation if not.
-    let normal_plane = Vec3::new(0., 1., 0.);
+    let rot_plane_norm = Vec3::new(0., 1., 0.);
 
     // The first bond vectors are defined as the anchor vec. These are Calpha's CP bond, Cp's N bond,
     // and N's Calpha bond. This is also the first sidechain bond out.
 
     unsafe {
+        println!("Calpha bonds:");
+        println!("C' {:?}", CALPHA_CP_BOND);
+        println!("N {:?}", CALPHA_N_BOND);
+        println!("R {:?}", CALPHA_R_BOND);
+
+
+        // println!("C' {:?}", CALPHA_N_BOND);
+    }
+
+    unsafe {
         // Find the second bond vectors. The initial anchor bonds (eg `CALPHA_CP_BOND`) are rotated
         // along the (underconstrained) normal plane above.
-        CALPHA_N_BOND = find_vec_in_plane(CALPHA_CP_BOND, BOND_ANGLE_CALPHA_N_CP, normal_plane);
-        CP_CALPHA_BOND = find_vec_in_plane(CP_N_BOND, BOND_ANGLE_CP_CALPHA_N, normal_plane);
-        N_CP_BOND = find_vec_in_plane(N_CALPHA_BOND, BOND_ANGLE_N_CP_CALPHA, normal_plane);
-        // todo: We don't have actual H measurements; using a dummy angle for now.
-        N_H_BOND = find_vec_in_plane(N_CALPHA_BOND, TAU * 2. / 3., normal_plane);
+        CALPHA_N_BOND = rotate_vec(CALPHA_CP_BOND, BOND_ANGLE_CALPHA_CP_N, rot_plane_norm);
+        CP_CALPHA_BOND = rotate_vec(CP_N_BOND, BOND_ANGLE_CP_N_CALPHA, rot_plane_norm);
+        N_CP_BOND = rotate_vec(N_CALPHA_BOND, BOND_ANGLE_N_CALPHA_CP, rot_plane_norm);
 
-        SIDECHAIN_BOND_OUT2 = find_vec_in_plane(ANCHOR_BOND_VEC, TAU / 3., normal_plane);
-        SIDECHAIN_BOND_TO_PREV = find_vec_in_plane(ANCHOR_BOND_VEC, TAU * 2. / 3., normal_plane);
+        // todo: We don't have actual H measurements; using a dummy angle for now.
+        // todo: Rather than assume it's in plane, we may need to apply the `find_third_bond_vec`
+        // todo logic below to find H atom locations.
+        N_H_BOND = rotate_vec(N_CALPHA_BOND, TAU * 2. / 3., rot_plane_norm);
+
+        SIDECHAIN_BOND_OUT2 = rotate_vec(ANCHOR_BOND_VEC, TAU / 3., rot_plane_norm);
+        SIDECHAIN_BOND_TO_PREV = rotate_vec(ANCHOR_BOND_VEC, TAU * 2. / 3., rot_plane_norm);
 
         // todo: To find the 3rd (and later 4th, ie hydrogen-to-atom) bonds, we we
         // todo taking an iterative approach based on that above. Long-term, you should
         // todo be able to calculate one of 2 valid choices (for carbon).
 
+
         CALPHA_R_BOND = find_third_bond_vec(
             CALPHA_CP_BOND,
-            BOND_ANGLE_CALPHA_N_CP,
+            CALPHA_N_BOND,
+            BOND_ANGLE_CALPHA_CP_R,
             BOND_ANGLE_CALPHA_N_R,
-            normal_plane,
+            rot_plane_norm,
+        );
+
+        CP_O_BOND = find_third_bond_vec(
+            CP_N_BOND,
+            CP_CALPHA_BOND,
+            BOND_ANGLE_CP_N_O,
+            BOND_ANGLE_CP_CALPHA_O,
+            rot_plane_norm,
         );
 
         // todo: The other ones.
-
-        /// Find the third bond by iterating over rotation axes, and using the one that provides
-        /// the closest match. For each rotation axis, we apply one bond angle as a constraint, and
-        /// attempt to minimize the second one.
-        fn find_third_bond_vec(
-            bond1: Vec3,
-            // bond2: Vec3,
-            angle_1_2: f64,
-            angle_1_3: f64,
-            starting_plane_norm: Vec3,
-        ) -> Vec3 {
-            // The angle between bond 2 and 3 must be within this (radians) to find our result.
-            const EPS: f64 = 0.01;
-            const NUM_AXES: usize = 10; // todo?
-            const AXIS_DIV: usize = 20; // todo?
-
-            let mut result = Vec3::new_zero();
-
-            for axis in 0..NUM_AXES {
-                let iterated_plane_norm =
-                    find_vec_in_plane(bond1, axis as f64 / NUM_AXES, starting_plane_norm);
-
-                // Rotate bond 1 around one of the available constraints: the bond angle between bonds
-                // 1 and 2.
-                result = find_vec_in_plane(bond1, angle_1_3, iterated_plane_norm);
-
-                // Measure the other constraint; the angle between bond 1 and our bond3 candidate.
-                let angle_1_3_meas = (bond1.dot(result)).acos();
-
-                // Compare the other constraint against the target; the bond 1-3 angle.
-                if (angle_1_3_meas - angle_1_3).abs() < EPS {
-                    break;
-                }
-            }
-
-            result
-        }
     }
 
     // Find vectors from C' to O, and Cα to R, given the previous 2 bonds for each.
