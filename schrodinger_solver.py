ca6638f6c57f867d32678c1da109595c853571e8
Data <the_alchemist@fastmail.com>
2022-12-18 16:10:02 -0300

    Sorted out an issue with the script

--- schrodinger_solver.py
+++ schrodinger_solver.py
@@ -16,6 +16,8 @@
 # Has some notable detractions about plane waves. Good info in general
 # comparing the 2.
 
+# https://chem.libretexts.org/Courses/University_of_California_Davis/UCD_Chem_107B%3A_Physical_Chemistry_for_Life_Scientists/Chapters/4%3A_Quantum_Theory/4.10%3A_The_Schr%C3%B6dinger_Wave_Equation_for_the_Hydrogen_Atom
+
 # Good resource overall?: http://davidbowler.github.io/AtomisticSimulations/index.html
 
 
@@ -30,7 +32,7 @@ from dataclasses import dataclass;
 import numpy as np
 import matplotlib.pyplot as plt
 
-from numpy import sqrt, pi
+from numpy import sqrt, pi, cos
 
 A_0 = 1.
 Z_H = 1.
@@ -56,16 +58,35 @@ def h_V(posit_nuc: Vec3, posit_sample: Vec3) -> float:
     diff = posit_sample - posit_nuc
     r = sqrt(diff.x**2 + diff.y**2 + diff.z**2)
 
-    return K_C * Q_PROT / r
+    # todo: Why negative? Seems we need it.
+    return -K_C * Q_PROT / r
 
 
-def h_s0_wf(posit_nuc: Vec3, posit_sample: Vec3, E: float) -> float:
+def h_wf_100(posit_nuc: Vec3, posit_sample: Vec3) -> float:
     """Analytic solution for n=1, s orbital"""
     diff = posit_sample - posit_nuc
     r = sqrt(diff.x**2 + diff.y**2 + diff.z**2)
 
     ρ = Z_H * r / A_0
-    return 1. / sqrt(pi) * (Z_H / A_0)**(3. / 2.) * np.exp(-ρ) * r**0
+    return 1. / sqrt(pi) * (Z_H / A_0)**(3. / 2.) * np.exp(-ρ)
+    # return 1. / sqrt(pi) * 1./ A_0**(3. / 2.) * np.exp(-ρ)
+
+
+def h_wf_200(posit_nuc: Vec3, posit_sample: Vec3) -> float:
+    diff = posit_sample - posit_nuc
+    r = sqrt(diff.x**2 + diff.y**2 + diff.z**2)
+
+    ρ = Z_H * r / A_0
+    return 1. / sqrt(32.*pi) * (Z_H / A_0)**(3. / 2.) * (2. - ρ) * np.exp(-ρ/2.)
+
+
+def h_wf_210(posit_nuc: Vec3, posit_sample: Vec3, theta: float) -> float:
+    diff = posit_sample - posit_nuc
+    r = sqrt(diff.x**2 + diff.y**2 + diff.z**2)
+
+    ρ = Z_H * r / A_0
+    return 1. / sqrt(32.*pi) * (Z_H / A_0)**(3. / 2.) * ρ * np.exp(-ρ/2.) * cos(theta)
+
 
 
 def main():
@@ -80,12 +101,13 @@ def main():
     # psi(r)'' = (E - V(r)) * 2*m/ħ**2 * psi(r)
     # psi(r) = (E - V(R))^-1 * ħ**2/2m * psi(r)''
 
-    E = -1/2 # n = 0
+    E = -1/2 # n = 1
 
     N = 10_000
 
 	# As a test iter, keep y and z = 0
 
+    V_vals = np.zeros(N)
     psi = np.zeros(N)
     psi_pp_expected = np.zeros(N)
     psi_pp_measured = np.zeros(N)
@@ -95,12 +117,17 @@ def main():
     # Smaller is more precise. Applies to dx, dy, and dz
     h = 0.00001  # aka dx?
 
+    wf = h_wf_100
+
     for i, x in enumerate(x_vals):
         posit_sample = Vec3(x, 0., 0.)
 
-        psi[i] = h_s0_wf(posit_nuc, posit_sample, E)
+        psi[i] = wf(posit_nuc, posit_sample)
 
+        # todo: Experimenting with negative sign here. Why?
         V = h_V(posit_nuc, posit_sample)
+        V_vals[i] = V
+
         psi_pp_expected[i] = (E - V) * -2. * M_ELEC / ħ**2 * psi[i]
 
         # Calculate psi'' based on a numerical derivative of psi
@@ -127,12 +154,12 @@ def main():
         z_prev = Vec3(posit_sample.x, posit_sample.y, posit_sample.z - h)
         z_next = Vec3(posit_sample.x, posit_sample.y, posit_sample.z + h)
 
-        psi_x_prev = h_s0_wf(posit_nuc, x_prev, E)
-        psi_x_next = h_s0_wf(posit_nuc, x_next, E)
-        psi_y_prev = h_s0_wf(posit_nuc, y_prev, E)
-        psi_y_next = h_s0_wf(posit_nuc, y_next, E)
-        psi_z_prev = h_s0_wf(posit_nuc, z_prev, E)
-        psi_z_next = h_s0_wf(posit_nuc, z_next, E)
+        psi_x_prev = wf(posit_nuc, x_prev)
+        psi_x_next = wf(posit_nuc, x_next)
+        psi_y_prev = wf(posit_nuc, y_prev)
+        psi_y_next = wf(posit_nuc, y_next)
+        psi_z_prev = wf(posit_nuc, z_prev)
+        psi_z_next = wf(posit_nuc, z_next)
 
         # Could combine these, but this split-by-axis is easier to understand.
 
@@ -140,16 +167,17 @@ def main():
         
         # Initialize using the second derivative temr
         psi_pp_measured[i] = 0.
-        psi_pp_measured[i] += psi_x_prev + psi_x_next - 2. * psi[i]
-        psi_pp_measured[i] += psi_y_prev + psi_y_next - 2. * psi[i]
-        psi_pp_measured[i] += psi_z_prev + psi_z_next - 2. * psi[i]
-        psi_pp_measured[i] /= h**2 * 1
+        psi_pp_measured[i] += (psi_x_prev + psi_x_next - 2. * psi[i])
+        psi_pp_measured[i] += (psi_y_prev + psi_y_next - 2. * psi[i])
+        psi_pp_measured[i] += (psi_z_prev + psi_z_next - 2. * psi[i])
+        psi_pp_measured[i] /= h**2
 
 		# psi_pp_measured[i] = 0.25 * psi_x_prev2 + psi_x_next2 + psi_y_prev2 + psi_y_next2 + \
 		# 	psi_z_prev2 + psi_z_next2 - 6. * psi[i]
 
     scaler = 1  # to make the plots show together.
 
+    plt.plot(x_vals, V_vals)
     plt.plot(x_vals, psi)
     plt.plot(x_vals, psi_pp_expected * scaler)  # Scaler for visualization
     plt.plot(x_vals, psi_pp_measured * scaler)
