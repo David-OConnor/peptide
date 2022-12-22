f75c804743a988e9b77b41e0f6f6b8d98b215556
Data <the_alchemist@fastmail.com>
2022-12-21 21:34:15 -0300

    Changed to 2d plot data

--- schrodinger_solver.py
+++ schrodinger_solver.py
@@ -55,8 +55,15 @@ class Vec3:
         return Vec3(self.x - other.x, self.y - other.y, self.z - other.z)
 
 
-def make_gaussian(x: ndarray, a: float, b: float, c: float) -> np.ndarray:
-    return a * exp(-(x - b)**2. / (2. * c**2))
+# def make_gaussian_1d(x: ndarray, a: float, b: float, c: float) -> np.ndarray:
+#     return a * exp(-(x - b)**2. / (2. * c**2))
+
+
+def make_gaussian_3d(posit_nuc, posit_sample: Vec3, a: float, b: float, c: float) -> float:
+    diff = posit_sample - posit_nuc
+    r = sqrt(diff.x**2 + diff.y**2 + diff.z**2)
+
+    return a * exp(-(r - b)**2. / (2. * c**2))
 
 
 def score_wf(target: ndarray, attempt: ndarray) -> float:
@@ -112,6 +119,10 @@ def main():
     # Let's stick to 3D. And try the H WF as a test that it
     # works, to validate our method.
 
+    # Set up 3d plot
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+
     posit_nuc = Vec3(0., 0., 0.)
 
     # Schrod eq for H: 
@@ -120,7 +131,7 @@ def main():
     # psi(r)'' = (E - V(r)) * 2*m/ħ**2 * psi(r)
     # psi(r) = (E - V(R))^-1 * ħ**2/2m * psi(r)''
 
-    N = 10_000
+    N = 150
 
 	# As a test iter, keep y and z = 0
 
@@ -128,76 +139,101 @@ def main():
     psi = np.zeros(N)
     psi_pp_expected = np.zeros(N)
     psi_pp_measured = np.zeros(N)
+
+    V_vals_2d = np.zeros((N, N))
+    psi_2d = np.zeros((N, N))
+    psi_pp_expected_2d = np.zeros((N, N))
+    psi_pp_measured_2d = np.zeros((N, N))
+
     x_vals = np.linspace(-10, 10, N)
+    y_vals = np.linspace(-10, 10, N)
 
     # Used for calculating numerical psi''.
     # Smaller is more precise. Applies to dx, dy, and dz
-    h = 0.00001  # aka dx?
+    h = 0.000001  # aka dx?
+
+    # Gauss consts.
+    # Here, we make up a Psi, numerically, to see how it converges.
+    a = 1./sqrt(pi)  # Height
+    b = 0.  # center
+    c = 1.5  # Width
 
     wf = h_wf_100
+    # wf = lambda poisit_nuc, posit_sample: make_gaussian_3d(posit_nuc, posit_sample, a, b, c)
+
     E = -1/2 # n = 1
     # E = -1/8 # n = 2
     # E = -0.05514706 # n = 3
 
-    # Here, we make up a Psi, numerically, to see how it converges.
-    a = 1./sqrt(pi)  # Height
-    b = 0.  # center
-    c = 5.  # Width
-    psi = make_gaussian(x_vals, a, b, c);
+
+
+    # todo: Your test wave functions need to be populated in 3 dimensions!
+
+    # psi = wf(posit_nuc, Vec3::n ))
 
     for i, x in enumerate(x_vals):
-        posit_sample = Vec3(x, 0., 0.)
+        for j, y in enumerate(y_vals):
+            posit_sample = Vec3(x, y, 0.)
 
-        # Note: If using an analytic etc psi, put this line back.
-        # psi[i] = wf(posit_nuc, posit_sample)
+            # Note: If using an analytic etc psi, put this line back.
+            # psi[i] = wf(posit_nuc, posit_sample)
+            psi_2d[i, j] = wf(posit_nuc, posit_sample)
 
-        # todo: Experimenting with negative sign here. Why?
-        V = V_h(posit_nuc, posit_sample)
-        V_vals[i] = V
+            # todo: Experimenting with negative sign here. Why?
+            V = V_h(posit_nuc, posit_sample)
+            # V_vals[i] = V
+            V_vals_2d[i, j] = V
 
-        psi_pp_expected[i] = (E - V) * -2. * M_ELEC / ħ**2 * psi[i]
+            # psi_pp_expected[i] = (E - V) * -2. * M_ELEC / ħ**2 * psi[i]
+            psi_pp_expected_2d[i, j] = (E - V) * -2. * M_ELEC / ħ**2 * psi_2d[i, j]
 
-        # Calculate psi'' based on a numerical derivative of psi
-        # in 3D.
+            # Calculate psi'' based on a numerical derivative of psi
+            # in 3D.
 
-        # This crude approach uses a 3d-block style grid. Maybe works for now.
-        # todo: Better, more spherical way of working this??
-        # This is currently a plus-style grid.
-        # todo: Also - reduce your approach to simplified operations by
-        # writing it out, or ref a prev doc that probably exists that you
-        # wrote it out on.
-        # We're 
+            # This crude approach uses a 3d-block style grid. Maybe works for now.
+            # todo: Better, more spherical way of working this??
+            # This is currently a plus-style grid.
+            # todo: Also - reduce your approach to simplified operations by
+            # writing it out, or ref a prev doc that probably exists that you
+            # wrote it out on.
+            # We're 
 
-        # todo: You need to subtract and scale by an appropriate dt on your adding
-        # todo and subtracting from x, y etc.
+            # todo: You need to subtract and scale by an appropriate dt on your adding
+            # todo and subtracting from x, y etc.
 
-        # todo: second deriv is perhaps avg of the above? Validate this.
-        # found in prev notes. Test on 1D, 2D, 3D.
+            # todo: second deriv is perhaps avg of the above? Validate this.
+            # found in prev notes. Test on 1D, 2D, 3D.
 
-        x_prev = Vec3(posit_sample.x - h, posit_sample.y, posit_sample.z)
-        x_next = Vec3(posit_sample.x + h, posit_sample.y, posit_sample.z)
-        y_prev = Vec3(posit_sample.x, posit_sample.y - h, posit_sample.z)
-        y_next = Vec3(posit_sample.x, posit_sample.y + h, posit_sample.z)
-        z_prev = Vec3(posit_sample.x, posit_sample.y, posit_sample.z - h)
-        z_next = Vec3(posit_sample.x, posit_sample.y, posit_sample.z + h)
+            x_prev = Vec3(posit_sample.x - h, posit_sample.y, posit_sample.z)
+            x_next = Vec3(posit_sample.x + h, posit_sample.y, posit_sample.z)
+            y_prev = Vec3(posit_sample.x, posit_sample.y - h, posit_sample.z)
+            y_next = Vec3(posit_sample.x, posit_sample.y + h, posit_sample.z)
+            z_prev = Vec3(posit_sample.x, posit_sample.y, posit_sample.z - h)
+            z_next = Vec3(posit_sample.x, posit_sample.y, posit_sample.z + h)
 
-        psi_x_prev = wf(posit_nuc, x_prev)
-        psi_x_next = wf(posit_nuc, x_next)
-        psi_y_prev = wf(posit_nuc, y_prev)
-        psi_y_next = wf(posit_nuc, y_next)
-        psi_z_prev = wf(posit_nuc, z_prev)
-        psi_z_next = wf(posit_nuc, z_next)
+            psi_x_prev = wf(posit_nuc, x_prev)
+            psi_x_next = wf(posit_nuc, x_next)
+            psi_y_prev = wf(posit_nuc, y_prev)
+            psi_y_next = wf(posit_nuc, y_next)
+            psi_z_prev = wf(posit_nuc, z_prev)
+            psi_z_next = wf(posit_nuc, z_next)
 
-        # Could combine these, but this split-by-axis is easier to understand.
+            # Could combine these, but this split-by-axis is easier to understand.
 
-        # todo: Is this how you compensate for h??. We removed the 1/4 term and added h^2.
-        
-        # Initialize using the second derivative temr
-        psi_pp_measured[i] = 0.
-        psi_pp_measured[i] += (psi_x_prev + psi_x_next - 2. * psi[i])
-        psi_pp_measured[i] += (psi_y_prev + psi_y_next - 2. * psi[i])
-        psi_pp_measured[i] += (psi_z_prev + psi_z_next - 2. * psi[i])
-        psi_pp_measured[i] /= h**2
+            # todo: Is this how you compensate for h??. We removed the 1/4 term and added h^2.
+            
+            # Initialize using the second derivative temr
+            # psi_pp_measured[i] = 0.
+            # psi_pp_measured[i] += (psi_x_prev + psi_x_next - 2. * psi[i])
+            # psi_pp_measured[i] += (psi_y_prev + psi_y_next - 2. * psi[i])
+            # psi_pp_measured[i] += (psi_z_prev + psi_z_next - 2. * psi[i])
+            # psi_pp_measured[i] /= h**2
+
+            psi_pp_measured_2d[i, j] = 0.
+            psi_pp_measured_2d[i, j] += (psi_x_prev + psi_x_next - 2. * psi_2d[i, j])
+            psi_pp_measured_2d[i, j] += (psi_y_prev + psi_y_next - 2. * psi_2d[i, j])
+            psi_pp_measured_2d[i, j] += (psi_z_prev + psi_z_next - 2. * psi_2d[i, j])
+            psi_pp_measured_2d[i, j] /= h**2
 
 		# psi_pp_measured[i] = 0.25 * psi_x_prev2 + psi_x_next2 + psi_y_prev2 + psi_y_next2 + \
 		# 	psi_z_prev2 + psi_z_next2 - 6. * psi[i]
@@ -208,12 +244,30 @@ def main():
 
     print(f"ψ'' Score: {score_wf(psi_pp_expected, psi_pp_measured)}")
 
-    plt.plot(x_vals, V_vals)
-    plt.plot(x_vals, psi)
-    plt.plot(x_vals, psi_pp_expected * scaler)  # Scaler for visualization
-    plt.plot(x_vals, psi_pp_measured * scaler)
+    # 1D data:
+    # plt.plot(x_vals, V_vals)
+    # plt.plot(x_vals, psi)
+    # plt.plot(x_vals, psi_pp_expected * scaler)  # Scaler for visualization
+    # plt.plot(x_vals, psi_pp_measured * scaler)
 
-    plt.ylim(-0.5, 0.8)
+    # plt.ylim(-0.5, 0.8)
+
+    
+
+    # todo: Default to larger window?
+
+    # 2d data:
+    x_plot, y_plot = np.meshgrid(x_vals, y_vals)
+
+    ax.plot_surface(x_plot, y_plot, V_vals_2d)
+    ax.plot_surface(x_plot, y_plot, psi_2d)
+    ax.plot_surface(x_plot, y_plot, psi_pp_expected_2d)
+    ax.plot_surface(x_plot, y_plot, psi_pp_measured_2d)
+
+    ax.set_aspect('equal')
+    ax.set_zlim(-0.5, 0.8)
+
+    
 
     plt.show()
 
